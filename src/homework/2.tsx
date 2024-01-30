35f79ae8900411a5c2147b2abbb8078952218809
kriandr79 <127450901+kriandr79@users.noreply.github.com>
2024-01-30 20:14:08 +0200

    ready

--- src/homework/2.tsx
+++ src/homework/2.tsx
@@ -1,5 +1,14 @@
 import React, {useReducer} from "react";
 
+interface State {
+  isRequestInProgress: boolean;
+  requestStep: 'idle' | 'start' | 'pending' | 'finished';
+}
+
+interface Action {
+  type: 'START_REQUEST' | 'PENDING_REQUEST' | 'FINISH_REQUEST' | 'RESET_REQUEST';
+}
+
 const initialState: State = {
   isRequestInProgress: false,
   requestStep: 'idle',
